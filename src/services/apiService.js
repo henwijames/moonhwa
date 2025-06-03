03c355ddd5855eced07a32eb039d473c7534dc27
Henry James Ribano <henryjamesribano27@gmail.com>
2025-06-03 15:11:18 +0800

    Update vercel.json to change API destination and refine CORS headers; remove user-agent, origin, and referer headers from apiService for improved security and compliance.

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -9,10 +9,6 @@ const apiService = axios.create({
   headers: {
     Accept: "application/json",
     "Accept-Language": "en-US,en;q=0.9",
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    Origin: "https://moonhwa.vercel.app",
-    Referer: "https://moonhwa.vercel.app/",
   },
 });
 
